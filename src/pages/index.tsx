072b37bb301cb5afdb0c44a1d3ee8c3a5a9d47b3
Sushant R. Dangal <srdarkseer@gmail.com>
2024-04-11 12:35:44 +0545

    setup: favicon and title of the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,24 @@ import React from "react";
 import WithLayout from "@/components/WithLayout";
 import MainLayout from "@/layouts/MainLayout";
 import Landing from "@/views/Landing";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <React.Fragment>
+      <Head>
+        <title>Synthetic AI</title>
+
+        <meta name="description" content="Synthetic AI" key="desc" lang="en" />
+
+        <meta
+          name="keywords"
+          content="ai, llm, synthetic, synthetic ai, syntheticai"
+          lang="en"
+        />
+
+        <link rel="icon" href="/sai-logo.svg" sizes="any" />
+      </Head>
       <WithLayout layout={MainLayout} component={Landing} />
     </React.Fragment>
   );
