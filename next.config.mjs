d881d923164811899bcf510e6391f11a8f8a616c
Sushant R. Dangal <srdarkseer@gmail.com>
2024-04-11 16:49:55 +0545

    setup: removed loader (gif not working)

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,14 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
-  images: {
-    loader: process.env.NODE_ENV === "production" ? "custom" : "default",
-    // Specify the loaderFile only if the custom loader is used
-    loaderFile:
-      process.env.NODE_ENV === "production"
-        ? "./src/utils/images/loader.ts"
-        : undefined,
-  },
+  // images: {
+  //   loader: process.env.NODE_ENV === "production" ? "custom" : "default",
+  //   // Specify the loaderFile only if the custom loader is used
+  //   loaderFile:
+  //     process.env.NODE_ENV === "production"
+  //       ? "./src/utils/images/loader.ts"
+  //       : undefined,
+  // },
 };
 
 export default nextConfig;
