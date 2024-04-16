49075296c637fbcf7fd023ebedc48ce9562b0847
Sushant R. Dangal <srdarkseer@gmail.com>
2024-04-16 18:54:47 +0545

    hotfix: resolved text issue

--- src/layouts/MainLayout/components/Banner.tsx
+++ src/layouts/MainLayout/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="w-full h-9 flex items-center justify-center bg-olive">
       <p className="text-white text-sm">
-        Please use on a Desktop - Mobile version coming soon?
+        Please use on a Desktop - Mobile version coming soon
       </p>
     </div>
   );
