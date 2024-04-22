4948b32b3b61337ba416a9789f0fe3443c09897d
Sushant R. Dangal <srdarkseer@gmail.com>
2024-04-22 09:47:33 +0545

    feature: updated contractABI

--- src/views/Landing/components/PopUpModal.tsx
+++ src/views/Landing/components/PopUpModal.tsx
@@ -133,50 +133,36 @@ const PopUpModal = ({ isOpen }: { isOpen: boolean }) => {
     ethersProvider = new ethers.providers.Web3Provider(wallet.provider, "any");
   }
 
-  const contractAddress = `${process.env.SAI_CONTRACT_ADDRESS}`; // SAI's Contract Address
+  // SAI's Contract Address
+  const contractAddress = `${process.env.SAI_CONTRACT_ADDRESS}`;
+
+  // SAI's Contract ABI
   const contractABI = [
-    // Dummy Contract ABI
+    { inputs: [], stateMutability: "nonpayable", type: "constructor" },
     {
+      anonymous: false,
       inputs: [
         {
-          internalType: "string",
-          name: "_name",
-          type: "string",
-        },
-        {
-          internalType: "uint256",
-          name: "_age",
-          type: "uint256",
-        },
-      ],
-      name: "createPerson",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
+          indexed: true,
           internalType: "address",
-          name: "_personAddress",
+          name: "owner",
           type: "address",
         },
-      ],
-      name: "getPerson",
-      outputs: [
         {
-          internalType: "string",
-          name: "name",
-          type: "string",
+          indexed: true,
+          internalType: "address",
+          name: "spender",
+          type: "address",
         },
         {
+          indexed: false,
           internalType: "uint256",
-          name: "age",
+          name: "value",
           type: "uint256",
         },
       ],
-      stateMutability: "view",
-      type: "function",
+      name: "Approval",
+      type: "event",
     },
     {
       anonymous: false,
@@ -184,25 +170,189 @@ const PopUpModal = ({ isOpen }: { isOpen: boolean }) => {
         {
           indexed: true,
           internalType: "address",
-          name: "personAddress",
+          name: "previousOwner",
           type: "address",
         },
         {
-          indexed: false,
-          internalType: "string",
-          name: "name",
-          type: "string",
+          indexed: true,
+          internalType: "address",
+          name: "newOwner",
+          type: "address",
         },
+      ],
+      name: "OwnershipTransferred",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        { indexed: true, internalType: "address", name: "to", type: "address" },
         {
           indexed: false,
           internalType: "uint256",
-          name: "age",
+          name: "value",
           type: "uint256",
         },
       ],
-      name: "PersonCreated",
+      name: "Transfer",
       type: "event",
     },
+    {
+      inputs: [{ internalType: "address", name: "wallet", type: "address" }],
+      name: "addExcludedWallet",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "owner", type: "address" },
+        { internalType: "address", name: "spender", type: "address" },
+      ],
+      name: "allowance",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "spender", type: "address" },
+        { internalType: "uint256", name: "amount", type: "uint256" },
+      ],
+      name: "approve",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "account", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "buyTax",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "uint256", name: "newBuyTax", type: "uint256" },
+        { internalType: "uint256", name: "newSellTax", type: "uint256" },
+      ],
+      name: "changeTax",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "decimals",
+      outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "maxTxAmount",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "openTrading",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "removeLimits",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "renounceOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "sellTax",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalSupply",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "recipient", type: "address" },
+        { internalType: "uint256", name: "amount", type: "uint256" },
+      ],
+      name: "transfer",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "sender", type: "address" },
+        { internalType: "address", name: "recipient", type: "address" },
+        { internalType: "uint256", name: "amount", type: "uint256" },
+      ],
+      name: "transferFrom",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+      name: "transferOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    { stateMutability: "payable", type: "receive" },
   ];
 
   let contract: ethers.Contract | null = null;
